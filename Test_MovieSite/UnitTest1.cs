f6d4375874d96d4ebad5077696631da259b331ac
EsraNurPalut <nuresrapalut@outlook.com>
2023-01-17 23:40:51 +0300

    Unit Test added.

---
+++ Test_MovieSite/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Movie.Site.Models;
+using System;
+using Xunit;
+
+namespace Test_MovieSite
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Create()
+        {
+           
+        }
+    }
+}
